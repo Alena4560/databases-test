b76e6020a1dc473a3b452338fa3b5fff0fd7621a
Алена Артамонова <artamonova-0202@list.ru>
2023-03-03 00:25:37 +0300

    Databases

---
+++ phones/management/commands/import_phones.py
@@ -0,0 +1,20 @@
+import csv
+
+from django.core.management.base import BaseCommand
+from phones.models import Phone
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        pass
+
+    def handle(self, *args, **options):
+        with open('phones.csv', 'r') as csvfile:
+
+            phone_reader = csv.DictReader(csvfile, delimiter=';')
+            next(phone_reader)
+            for line in phone_reader:
+                phone = Phone(id=line['id'], name=line['name'],
+                              image=line['image'], price=line['price'],
+                              release_date=line['release_date'], lte_exists=line['lte_exists'])
+                phone.save()
